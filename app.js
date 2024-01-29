032487f7a5a2c3ba41b32b6dfb12d0040a18ebe7
iisshh17 <itsmeishmish@gmail.com>
2024-01-29 16:12:42 +0530

    successfully done

---
+++ app.js
@@ -0,0 +1,11 @@
+class makeContainer extends React.Component {
+  render() {
+      return React.createElement(`div`, null, `Hey Kalvians`,
+          React.createElement(`div`, null, `Let's rock and roll with classes`)
+      );
+  }
+
+}
+
+const container = document.getElementById("react-container")
+ReactDOM.render(React.createElement(makeContainer), container);
